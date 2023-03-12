dcdcc67f58243bb5245338ea5318546893af5a94
mtserik <erik.stos.mts@gmail.com>
2023-03-12 11:18:49 -0300

    Adicionando a biblioteca ReactNavigation ao projeto

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StatusBar, SafeAreaView } from 'react-native';
 import useProdutores from './src/hooks/useProdutores';
+import { NavigationContainer } from '@react-navigation/native';
 
 import Home from './src/telas/Home';
 import Cesta from './src/telas/Cesta';
@@ -10,13 +11,15 @@ export default function App() {
 
   return <SafeAreaView style={{ flex: 1 }}>
     <StatusBar />
-    <Home melhoresProdutores={false} />
-    {/* {produtores.length > 0 && 
-      <Cesta produtor={{
-        nome: produtores[0].nome, 
-        imagem: produtores[0].imagem
-      }}
-      {...produtores[0].cestas[0]} />
-    } */}
+    <NavigationContainer>
+      <Home melhoresProdutores={false} />
+      {/* {produtores.length > 0 && 
+        <Cesta produtor={{
+          nome: produtores[0].nome, 
+          imagem: produtores[0].imagem
+        }}
+        {...produtores[0].cestas[0]} />
+      } */}
+    </NavigationContainer>
   </SafeAreaView>
 }
