668001c9c1145c0b1cce829582d3e63785a10d29
mtserik <erik.stos.mts@gmail.com>
2023-03-12 14:55:08 -0300

    Finalizando projeto

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -3,15 +3,35 @@ import React from "react";
 import { NavigationContainer, TabActions } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import Home from '../telas/Home';
-import MelhoresProdutores from '../telas/MelhoresProdutores';
+import ProdutorRotas from "./ProdutorRotas";
+import MelhoresProdutoresRotas from "./MelhoresProdutoresRotas";
+
+import Coracao from "../assets/coracao.svg";
+import Home from "../assets/home.svg";
+
 const Tab = createBottomTabNavigator();
 
 export default function AppRotas() {
     return <NavigationContainer>
-            <Tab.Navigator>
-            <Tab.Screen  name='Home' component={Home} />
-            <Tab.Screen name='Melhores Produtores' component={MelhoresProdutores} />
-            </Tab.Navigator>
+        <Tab.Navigator screenOptions={ ({ route }) => ({ 
+            headerShown: false,
+            tabBarIcon: ({ color }) => {
+                let Icon = Home;
+
+                if (route.name === "Melhores Produtores") {
+                    Icon = Coracao;
+                }
+
+                return <Icon color={color} width={20} height={20}/>
+            },
+            tabBarActiveTintColor: "#2A9F85",
+            tabBarInactiveTintColor: "#C7C7C7",
+            tabBarLabelStyle: {
+                fontSize: 15,
+            },
+        })}>
+        <Tab.Screen  name='Home' component={ProdutorRotas} />
+        <Tab.Screen name='Melhores Produtores' component={MelhoresProdutoresRotas} />
+        </Tab.Navigator>
     </NavigationContainer>
 }
--- src/telas/Produtores/index.js
+++ src/telas/Produtores/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import Produtor from './componentes/Produtor';
 import Topo from './componentes/Topo';
@@ -7,12 +8,36 @@ import useProdutores from '../../hooks/useProdutores';
 import useTextos from '../../hooks/useTextos';
 
 export default function Produtores({ melhoresProdutores }) {
+  const navigation = useNavigation();
+  const route = useRoute();
+  
+  const [ exibeMensagem, setExibeMensagem ] = useState(false);
+
   const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
+  const { tituloProdutores, mensagemCompra } = useTextos();
+
+  const nomeCompra = route.params?.compra.nome;
+  const timestampCompra = route.params?.compra.timestamp;
+  const mensagemCompleta = mensagemCompra?.replace('$NOME', nomeCompra);
+
+  useEffect( () => {
+    setExibeMensagem(!!nomeCompra);
+    let timeout;
+
+    if (nomeCompra) {
+      timeout = setTimeout(()=>{
+        setExibeMensagem(false);
+      }, 3000);
+    }
+
+    return () => clearTimeout(timeout);
+
+  }, [timestampCompra])
 
   const TopoLista = () => {
     return <>
       <Topo melhoresProdutores={melhoresProdutores} />
+      { exibeMensagem && <Text style={estilos.compra}>{ mensagemCompleta }</Text> }
       <Text style={estilos.titulo}>{tituloProdutores}</Text>
     </>
   }
@@ -20,7 +45,9 @@ export default function Produtores({ melhoresProdutores }) {
   return <FlatList
     data={lista}
     renderItem={
-      ({ item }) => <Produtor {...item} aoPressionar={() => {}} />
+      ({ item }) => <Produtor {...item} aoPressionar={() => {
+        navigation.navigate("Produtor", item);
+      }} />
     }
     keyExtractor={({ nome }) => nome}
     ListHeaderComponent={TopoLista}
@@ -38,5 +65,12 @@ const estilos = StyleSheet.create({
     marginTop: 16,
     fontWeight: 'bold',
     color: '#464646',
+  },
+  compra: {
+    backgroundColor: "#EAF5F3",
+    padding: 16,
+    color: "#464646",
+    fontSize: 16,
+    lineHeight: 26,
   }
 })
